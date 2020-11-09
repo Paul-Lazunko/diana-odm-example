922d368cfb8cb702ab0e1cd7555f9db111cb7e29
pashaman <plavel@gmail.com>
2020-11-09 17:00:10 +0700

    [hotfix] fixed settings

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import { odm } from './odm';
 
 async function start () {
-  await odm.connect();
-  const userModel = odm.getModel('user');
-  const postModel = odm.getModel('post');
-  // Do something
-  odm.disconnect()
+ try {
+   await odm.connect();
+   const userModel = odm.getModel('user');
+   const postModel = odm.getModel('post');
+
+   // Do something
+
+   odm.disconnect()
+ } catch(e) {
+   console.log(e);
+ }
 }
 
 start().catch(error => console.log);

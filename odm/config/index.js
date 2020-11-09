35821b5e36b6dd5d127d4949143600582ac6d9b2
pashaman <plavel@gmail.com>
2020-11-09 09:16:43 +0700

    [hotfix] made key the same as at the docs (for stupid guys who dont read docs)

--- odm/config/index.js
+++ odm/config/index.js
@@ -1,7 +1,7 @@
 export const clientOptions = {
   host: 'localhost',
   port: 34567,
-  secureKey: 'someSecureKey',
+  secureKey: 'myAwesomeSecretKey',
   doReconnectOnClose: true,
   requestTimeout: 10000,
   reconnectInterval: 5000
